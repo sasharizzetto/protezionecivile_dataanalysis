4b549506164bb94097d0d25e8fb476f8bbb9a9d3
sasharizzetto <sasha.rizzetto@gmail.com>
2021-02-12 17:48:04 +0100

    Alcuni grafici su comuni fatti

---
+++ file.R
@@ -0,0 +1,33 @@
+install.packages("dbplyr")
+install.packages("RPostgreSQL")
+install.packages("DT")
+
+library(dplyr)
+library(RPostgreSQL)
+
+con <- DBI::dbConnect(RPostgreSQL::PostgreSQL(), 
+                      host = "0.tcp.ngrok.io",
+                      port = "12696",
+                      dbname = "pc", 
+                      user = "postgres",
+                      password = "postgres")
+                      #password = rstudioapi::askForPassword("postgres"))
+
+#con <- dbConnect(drv, dbname="pc", host="", port="", user="", password="")
+
+# tbl <- dplyr::tbl(con, dbplyr::in_schema('mortalidad','def0307'))
+
+tb = tbl(con, "attivita")
+
+# tb %>% show_query()
+
+tb %>% left_join(tbl(con, "attivita_completata")) %>% show_query()
+
+# 1. ATTIVITA PER COMUNE
+att_per_comune = tbl(con, "attivita_completata") %>% 
+  right_join(tbl(con, "attivita_in_comune")) %>% 
+  count(nome_comune) %>% 
+  collect()
+
+
+
